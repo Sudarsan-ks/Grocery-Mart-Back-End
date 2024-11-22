eab7133e55c31b2db729222b660ec767c3e517de
Sudarsan-ks <sansudar908@gmail.com>
2024-11-22 19:24:47 +0530

    new updation

--- models/addToCart.js
+++ models/addToCart.js
@@ -6,7 +6,7 @@ const cartItemSchema = mongoose.Schema({
     ref: "grocery",
     required: true,
   },
-  quantity: { type: Number, required: true },
+  quantity: { type: Number, required: true, default: 1 },
   price: { type: Number, required: true },
 });
 
