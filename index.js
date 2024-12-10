39d4ef9a1c6c75be3cb6a87bc9c23ced2cfe7a8d
Sudarsan-ks <sansudar908@gmail.com>
2024-12-10 18:41:14 +0530

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 const corsOptions = {
-  origin: [process.env.CLIENT_URL],
+  origin: process.env.CLIENT_URL,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
