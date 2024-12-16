43a86a6021cc29f1f011b4e9a640849c43f8c96b
Sudarsan-ks <sansudar908@gmail.com>
2024-12-16 06:36:28 +0530

    update

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -45,7 +45,7 @@ router.post("/verifyPayment", async (req, res) => {
   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
     req.body;
   try {
-    const generateSignature = crypto
+    const generateSignature =await crypto
       .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
       .update(`${razorpay_order_id}|${razorpay_payment_id}`)
       .digest("hex");
