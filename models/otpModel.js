69cc8da82797fb5f33146cead9e469b5ffe8db36
Sudarsan-ks <sansudar908@gmail.com>
2024-12-29 01:58:18 +0530

    Update

--- models/otpModel.js
+++ models/otpModel.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const otpSchema = mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    otp: { type: String, required: true },
+    otp: { type: Number, required: true },
   },
   {
     timestamps: true,
   }
 );
+otpSchema.index({ createdAt: 1 }, { expireAfterSeconds: 500 });
 
 const Otp = mongoose.model("Otp", otpSchema);
 module.exports = Otp;
